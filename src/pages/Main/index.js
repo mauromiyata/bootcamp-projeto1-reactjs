82c88fd9f66be40acd8b206603a2a26ddc4f807c
Mauro Toshio Miyata <mauromiyata@gmail.com>
2019-07-07 22:03:26 -0300

    adicionado roteamento e estilo da página main

---
+++ src/pages/Main/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { FaGitAlt, FaPlus } from 'react-icons/fa';
+
+import { Container, Form, SubmitButton } from './style';
+
+export default function Main() {
+  return (
+    <Container>
+      <h1>
+        <FaGitAlt />
+        Repositórios
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar repositório" />
+
+        <SubmitButton disable>
+          <FaPlus color="#fff" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
+}
